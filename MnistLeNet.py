0d3b557cb6cbf184d33d0cd9b0e288fa7d996403
Ruoshi Liu <rl3111@columbia.edu>
2020-05-09 18:25:59 -0400

    smaller Lenet-5 model

--- MnistLeNet.py
+++ MnistLeNet.py
@@ -7,12 +7,11 @@ class MnistLeNet(nn.Module):
         super(MnistLeNet, self).__init__()
         # 1 input image channel, 6 output channels, 3x3 square convolution
         # kernel
-        self.conv1 = nn.Conv2d(1, 6, 3)
-        self.conv2 = nn.Conv2d(6, 16, 3)
+        self.conv1 = nn.Conv2d(1, 2, 3)
+        self.conv2 = nn.Conv2d(2, 3, 3)
         # an affine operation: y = Wx + b
-        self.fc1 = nn.Linear(16 * 6 * 6, 120)  # 6*6 from image dimension
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc1 = nn.Linear(3 * 6 * 6, 5)  # 6*6 from image dimension
+        self.fc2 = nn.Linear(5, 10)
 
     def forward(self, x):
         # Max pooling over a (2, 2) window
@@ -21,8 +20,7 @@ class MnistLeNet(nn.Module):
         x = F.max_pool2d(F.relu(self.conv2(x)), 2)
         x = x.view(-1, self.num_flat_features(x))
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.fc2(x)
         return x
 
     def num_flat_features(self, x):
