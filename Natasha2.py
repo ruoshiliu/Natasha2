52bdba3a3f98b32cb6bf785fb94dd5d4be34f1b0
Yihao Li <alexliyihao@gmail.com>
2020-05-10 17:35:55 -0400

    Natasha2 update

--- Natasha2.py
+++ Natasha2.py
@@ -10,13 +10,13 @@ class Natasha2(torch.optim.Optimizer):
         if B < 0.0:
             raise ValueError("Invalid B value: {}".format(B))
         if p < 0.0:
-            raise ValueError("Invalid p value: {}".format(p)) 
+            raise ValueError("Invalid p value: {}".format(p))
         if sigma < 0.0:
-            raise ValueError("Invalid sigma value: {}".format(sigma)) 
+            raise ValueError("Invalid sigma value: {}".format(sigma))
         if delta < 0.0:
-            raise ValueError("Invalid delta value: {}".format(delta)) 
+            raise ValueError("Invalid delta value: {}".format(delta))
         if eta < 0.0:
-            raise ValueError("Invalid eta value: {}".format(eta)) 
+            raise ValueError("Invalid eta value: {}".format(eta))
         self.delta = delta
         self.eta = eta
         defaults = dict(params=params, alpha=alpha, B=B, p=p, sigma=sigma, delta=delta, eta=eta)
@@ -29,37 +29,35 @@ class Natasha2(torch.optim.Optimizer):
     @torch.no_grad()
     def step(self, hessian_matrix):
         hess = -hessian_matrix
-        v = torch.rand(hess.size()[0],1)
+        v = torch.randn([hess.size()[0],1],dtype = torch.float32)
         Mi = torch.ones_like(hess) + self.eta * hess
         for i in range(int(1/(self.delta ** 2))):
-            v = torch.matmul(Mi, v)
-        v = v / torch.norm(v)
-        if (torch.chain_matmul(v.transpose(0,1), hess, v) <= -self.delta /2):
+            v = torch.mm(Mi, v)
+            v = v / torch.norm(v)
+        kick_criterion = torch.chain_matmul(v.transpose(0,1), hess, v)
+        print(kick_criterion.item())
+
+        if (kick_criterion <= -self.delta /2):
             print('kick')
             kick = self.bern.sample()*2-1
             for group in self.param_groups:
                 for ctr, p in enumerate(group['params']):
                     if p.grad is None:
                         continue
-                    p.add_(v[ctr], alpha = kick*group['delta'])
+                    additive = (kick*group['delta']*v[ctr]).cuda()
+                    p.add_(additive)
         else:
-            print('not kick')
             for group in self.param_groups:
                 for p in group['params']:
                     if p.grad is None:
                         continue
-                    d_p = p.grad.view(-1)
-                    x_h = p.view(-1)
+                    d_p = p.grad
+                    x_h = p
                     for s in range(group['p']):
-                        x_0 = x_h
-                        x_list = []
-                        x_list.append(x_0)
+                        x_list = [x_h]
                         for t in range(int(group['B']/group['p'])):
                             d_tmp = d_p + 2*group['sigma']*(x_list[t] - x_h)
-                            x_list.append(x_list[t] - group['alpha'] * d_tmp)
+                            x_list.append(x_list[t].add_(d_tmp, alpha = -group['alpha']))
                         temp = torch.stack(x_list)
                         x_h = torch.mean(temp, dim=0)
-                    x_h = x_h.reshape(p.shape)
-
-                    p.eq_(x_h)
-
+                    p=x_h
